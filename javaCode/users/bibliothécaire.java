72cf56ef3e1ca8e8d57fd0a2ce095765c1a04a46
sanaaBens <bensmaa.sanaa@gmail.com>
2022-04-20 19:23:33 +0200

    Create bibliothécaire.java

---
+++ javaCode/users/bibliothécaire.java
@@ -0,0 +1,19 @@
+package users;
+
+import java.util.ArrayList;
+
+public class bibliothécaire {
+
+	private int idBib;
+	private String nom;
+	private String prenom;
+
+	public ArrayList<livre> lister() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void enregistrerEmprunt() {
+		throw new UnsupportedOperationException();
+	}
+
+}
