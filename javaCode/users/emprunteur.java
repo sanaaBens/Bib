84dd72b614d9ecc11cae8fa227e5f80ce5d89507
sanaaBens <bensmaa.sanaa@gmail.com>
2022-04-20 19:23:37 +0200

    Create emprunteur.java

---
+++ javaCode/users/emprunteur.java
@@ -0,0 +1,26 @@
+package users;
+
+public class emprunteur extends  livre{
+
+	private int idEmp;
+	private String nom;
+	private String prenom;
+	private String CIN;
+
+	/**
+	 * 
+	 * @param T
+	 */
+	public boolean chercher(titre T) {
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param L
+	 */
+	public void emprunter(livre L) {
+		throw new UnsupportedOperationException();
+	}
+
+}
