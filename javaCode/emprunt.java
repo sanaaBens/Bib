a12c784748269c73f8af129e8bcd31c08b267a16
sanaaBens <bensmaa.sanaa@gmail.com>
2022-04-20 19:18:14 +0200

    Create emprunt.java

---
+++ javaCode/emprunt.java
@@ -0,0 +1,11 @@
+import java.utils.Date;
+
+public class emprunt (dateRetour Date){
+
+	private Date dateEmprunt;
+	private Date dateRetour;
+	
+	dateEmprunt = LocalDateTime. now();
+	this.dateRetour = dateRetour;
+
+}
